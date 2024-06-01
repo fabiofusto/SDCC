45f5e304a8e46c9a8fc5f850b4e30d444d5dae4d
fabiofusto <fabio.fusto@gmail.com>
2024-06-01 20:09:55 +0200

    reverted last commit and modified allowed headers

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,7 +14,8 @@ const { auth } = NextAuth(authConfig);
  const corsHeaders: {[key: string]: string} = {
   'Access-Control-Allow-Origin': '*', // Permetti tutte le origini, modificalo secondo le tue necessità
   'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+  'Access-Control-Allow-Credentials': 'true'
 };
 
 export default auth((req) => {
