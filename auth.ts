5fec090927bdc61c60fe0ce6eb65e0abdee7bfb4
fabiofusto <fabio.fusto@gmail.com>
2024-06-03 14:54:09 +0200

    added pkce cookie configs

--- auth.ts
+++ auth.ts
@@ -4,20 +4,17 @@ import { db } from '@/lib/db';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  debug: true,
-  logger: {
-    error: console.error,
-    warn: console.warn,
-    info: console.log,
-    debug: console.log,
-  },
-  trustHost: true,
-  events: {
-    async linkAccount({ user }) {
-      await db.user.update({
-        where: { id: user.id },
-        data: { emailVerified: new Date() },
-      });
+  adapter: PrismaAdapter(db),
+  session: { strategy: 'jwt' },
+  cookies: {
+    pkceCodeVerifier: {
+      name: "authjs.pkce.code_verifier",
+      options: {
+        httpOnly: true,
+        sameSite: "none",
+        path: "/",
+        secure: process.env.NODE_ENV === "production",
+      },
     },
   },
   callbacks: {
@@ -26,23 +23,25 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     },
     async session({ token, session }) {
       if (token.sub && session.user) session.user.id = token.sub;
-
+      
       return session;
     },
   },
-  cookies: {
-    pkceCodeVerifier: {
-      name: "next-auth.pkce.code_verifier",
-      options: {
-        httpOnly: true,
-        sameSite: "none",
-        path: "/",
-        secure: true,
-      },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
     },
   },
-  redirectProxyUrl: "https://cobrainsights.xyz/api/auth",
-  adapter: PrismaAdapter(db),
-  session: { strategy: 'jwt' },
+  debug: true,
+  logger: {
+    error: console.error,
+    warn: console.warn,
+    info: console.log,
+    debug: console.log,
+  },
+  trustHost: true,
   ...authConfig,
 });
