07e52650079f0d59e7fdcdecd6602bc798a403d6
fabiofusto <fabio.fusto@gmail.com>
2024-06-07 17:22:35 +0200

    removed callbacks and events

--- auth.ts
+++ auth.ts
@@ -7,34 +7,34 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   
-  callbacks: {
-    async jwt({ token }) {
-      return token;
-    },
-    async session({ token, session }) {
-      if (token.sub && session.user) session.user.id = token.sub;
+  // callbacks: {
+  //   async jwt({ token }) {
+  //     return token;
+  //   },
+  //   async session({ token, session }) {
+  //     if (token.sub && session.user) session.user.id = token.sub;
 
-      return session;
-    },
-    async signIn({user, account, profile }) {
-      if (account?.provider === 'cognito' && profile?.email_verified) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-    async redirect({url, baseUrl}) {
-      return baseUrl;
-    },
-  },
-  events: {
-    async linkAccount({ user }) {
-      await db.user.update({
-        where: { id: user.id },
-        data: { emailVerified: new Date() },
-      });
-    },
-  },
+  //     return session;
+  //   },
+  //   async signIn({user, account, profile }) {
+  //     if (account?.provider === 'cognito' && profile?.email_verified) {
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+  //   },
+  //   async redirect({url, baseUrl}) {
+  //     return baseUrl;
+  //   },
+  // },
+  // events: {
+  //   async linkAccount({ user }) {
+  //     await db.user.update({
+  //       where: { id: user.id },
+  //       data: { emailVerified: new Date() },
+  //     });
+  //   },
+  // },
   debug: process.env.NODE_ENV === 'development',
   ...authConfig,
 });
