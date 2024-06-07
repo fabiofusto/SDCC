bff929564d30f10e7310391e8d827ffebd963676
fabiofusto <fabio.fusto@gmail.com>
2024-06-07 15:47:13 +0200

    changed signIn for analyze route

--- src/app/analyze/layout.tsx
+++ src/app/analyze/layout.tsx
@@ -1,20 +1,24 @@
-import {MaxWidthWrapper} from '@/components/max-width-wrapper'
-import {Steps} from '@/components/steps'
-import { ReactNode } from 'react'
-import { auth, signIn } from '../../../auth'
-import { redirect } from 'next/navigation'
-import { authRoutes, uploadRoute } from '../../../routes'
+import { MaxWidthWrapper } from '@/components/max-width-wrapper';
+import { Steps } from '@/components/steps';
+import { ReactNode } from 'react';
+import { auth, signIn } from '../../../auth';
 
 const Layout = async ({ children }: { children: ReactNode }) => {
-  const session = await auth()
-  if(!session || !session.user) await signIn('cognito', { callbackUrl: uploadRoute })
+  const session = await auth();
+  if (!session || !session.user) {
+    try {
+      await signIn('cognito');
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
-    <MaxWidthWrapper className='flex-1 flex flex-col'>
+    <MaxWidthWrapper className="flex-1 flex flex-col">
       <Steps />
       {children}
     </MaxWidthWrapper>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
